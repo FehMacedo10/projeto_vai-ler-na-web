d8399648a8ccb1c6d376d8582ee8e21c5f873a21
FehMacedo10 <fmacedoribeiro@outlook.com>
2025-04-09 17:53:21 -0300

    feat: atualizar a estrutura do projeto e adicionar dependências do axios

--- src/components/pages/livrosDoados/livrosDoados.jsx
+++ src/components/pages/livrosDoados/livrosDoados.jsx
@@ -1,58 +1,43 @@
-import livroAlice from '../../../assets/Alice no País das Maravilhas.jpg';
-import livroComoEu from '../../../assets/Como eu era antes de você.jpg';
-import livroHarryPotter from '../../../assets/Harry Potter.jpg';
-import livroJogoDoAnjo from '../../../assets/O Jogo do Anjo.jpg';
-import livroItACoisa from '../../../assets/It A coisa.jpg';
-import s from './livrosDoados.module.scss';
+import livroAlice from "../../../assets/Alice no País das Maravilhas.jpg";
+import livroComoEu from "../../../assets/Como eu era antes de você.jpg";
+import livroHarryPotter from "../../../assets/Harry Potter.jpg";
+import livroJogoDoAnjo from "../../../assets/O Jogo do Anjo.jpg";
+import livroItACoisa from "../../../assets/It A coisa.jpg";
+import s from "./livrosDoados.module.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 // Componente de livros doados que contém a lista de livros disponíveis para doação
 export default function LivrosDoados() {
-    return (
-        <section className={s.livrosDoadosSection}>
-            <h2>Livros Doados</h2>
-            <section className={s.containerCards}>
-                <section>
-                    <img src={livroAlice} alt="Imagem da capa do Livro Alice no País das Maravilhas" />
-                    <div>
-                        <h3>Alice no País das Maravilhas</h3>
-                        <p>Lewis Carroll</p>
-                        <p>Ficção</p>
-                    </div>
-                </section>
-                <section>
-                    <img src={livroComoEu} alt="Imagem da capa do Livro Como eu era antes de você" />
-                    <div>
-                        <h3>Como eu era antes de você</h3>
-                        <p>Jojo Moyes</p>
-                        <p>Comédia Romântica</p>
-                    </div>
-                </section>
-                <section>
-                    <img src={livroHarryPotter} alt="Imagem da capa do Livro Harry Potter e o Cálice de Fogo" />
-                    <div>
-                        <h3>Harry Potter e o Cálice de Fogo</h3>
-                        <p>J.K. Rowling</p>
-                        <p>Ficção</p>
-                    </div>
-                </section>
-                <section>
-                    <img src={livroJogoDoAnjo} alt="Imagem da capa do Livro O Jogo do Anjo" />
-                    <div>
-                        <h3>O Jogo do Anjo</h3>
-                        <p>Carlos Ruiz Záfon</p>
-                        <p>Ficção Histórica</p>
-                    </div>
-                </section>
-                <section>
-                    <img src={livroItACoisa} alt="Imagem da capa do Livro It: A Coisa" />
-                    <div>
-                        <h3>It: A Coisa</h3>
-                        <p>Stephen King</p>
-                        <p>Ficção</p>
-                    </div>
-                </section>
-            </section>
-            
-        </section>
+  const [livros, setLivros] = useState([]);
+
+  const listarLivros = async () => {
+    const resposta = await axios.get(
+      "https://projeto-api-vai-ler-na-web.onrender.com/livros "
     );
+
+    setLivros(resposta.data);
+  };
+
+  useEffect(() => {
+    listarLivros();
+  }, []);
+
+  return (
+    <section className={s.livrosDoadosSection}>
+      <h2>Livros Doados</h2>
+      <section className={s.containerCards}>
+        {livros.map((item) => (
+          <section>
+            <img src={item.imagem_url} alt={`Título do livro ${item.titulo}`} />
+            <div>
+              <h3>{item.titulo}</h3>
+              <p>{item.autor}</p>
+              <p>{item.categoria}</p>
+            </div>
+          </section>
+        ))}
+      </section>
+    </section>
+  );
 }
--- src/components/pages/queroDoar/queroDoar.jsx
+++ src/components/pages/queroDoar/queroDoar.jsx
@@ -1,22 +1,89 @@
-import iconeLivro from '../../../assets/Icone Livro.png';
-import s from './queroDoar.module.scss';
+import { useState } from "react";
+import iconeLivro from "../../../assets/Icone Livro.png";
+import s from "./queroDoar.module.scss";
+import axios from "axios";
 
 // Componente de formulário de doação que permite aos usuários enviar informações sobre o livro que desejam doar
 export default function QueroDoar() {
-    return (
-        <section className={s.queroDoarSection}>
-            <p>Por favor, preencha o formulário com suas informações e as informações do Livro.</p>
-            <form action="">
-                <div>
-                    <img src={iconeLivro} alt="Imagem com ícone de um livro aberto com borda azul" />
-                    <h2>Informações do Livro</h2>
-                </div>
-                <input type="text" name="" id="" placeholder="Título" />
-                <input type="text" name="" id="" placeholder="Categoria" />
-                <input type="text" name="" id="" placeholder="Autor" />
-                <input type="text" name="" id="" placeholder="Link da Imagem" />
-                <input type="submit" value="Doar" className={s.buttonDoar} />
-            </form>
-        </section>
-    );
+  const [titulo, setTitulo] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [autor, setAutor] = useState("");
+  const [imagem_url, setImagem_Url] = useState("");
+
+  const capturarTitulo = (e) => {
+    setTitulo(e.target.value);
+  };
+
+  const capturarCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+
+  const capturarAutor = (e) => {
+    setAutor(e.target.value);
+  };
+
+  const capturarImagem_Url = (e) => {
+    setImagem_Url(e.target.value);
+  };
+
+  const enviarDados = async () => {
+    const endpointApi = "https://projeto-api-vai-ler-na-web.onrender.com/doar";
+
+    const dadosAEnviar = {
+      titulo,
+      categoria,
+      autor,
+      imagem_url,
+    };
+
+    await axios.post(endpointApi, dadosAEnviar);
+  };
+
+  return (
+    <section className={s.queroDoarSection}>
+      <p>
+        Por favor, preencha o formulário com suas informações e as informações
+        do Livro.
+      </p>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div>
+          <img
+            src={iconeLivro}
+            alt="Imagem com ícone de um livro aberto com borda azul"
+          />
+          <h2>Informações do Livro</h2>
+        </div>
+        <input
+          type="text"
+          placeholder="Título"
+          onChange={capturarTitulo}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Categoria"
+          onChange={capturarCategoria}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Autor"
+          onChange={capturarAutor}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Link da Imagem"
+          onChange={capturarImagem_Url}
+          required
+        />
+        <input
+          type="submit"
+          value="Doar"
+          className={s.buttonDoar}
+          onClick={enviarDados}
+        />
+      </form>
+    </section>
+  );
 }
